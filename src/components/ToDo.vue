740c8f8fb4674ebed76a263346837769ffc5300c
brunosm16 <brunoskr23@gmail.com>
2022-08-10 11:42:26 -0300

    feat: add to-do component.

---
+++ src/components/ToDo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="to-do">
+    <CurrentTime class="col-3" />
+    <TaskInput class="col-6" @add-task="pushTask" />
+  </div>
+</template>
+
+<script>
+import CurrentTime from "./CurrentTime.vue";
+import TaskInput from "./TaskInput.vue";
+
+export default {
+  name: "ToDo",
+
+  components: {
+    CurrentTime,
+    TaskInput,
+  },
+
+  data: () => ({
+    tasks: [],
+  }),
+
+  methods: {
+    pushTask(task) {
+      this.tasks.push({
+        task,
+        createdAt: Date.now(),
+        updatedAt: undefined,
+        finishedAt: undefined,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.to-do {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 30px;
+  gap: 50px;
+}
+</style>
